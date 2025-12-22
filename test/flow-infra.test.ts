15069b6b8e87282a11d1c6e2b23f447030586943
Oscar Carlek <osca3666@colorado.edu>
2025-12-22 14:45:58 -0700

    Initial commit

---
+++ test/flow-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FlowInfra from '../lib/flow-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/flow-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FlowInfra.FlowInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
